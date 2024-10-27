be578f3e8bf1e1211e209c89cc5b4eab4a0a5dc1
William Warfield <179273497+wwarfield@users.noreply.github.com>
2024-10-27 08:31:52 -0500

    maturin init again

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn ffi_python(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
