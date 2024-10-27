cdc793b05f514268d37d9188891f1ad86aa09c43
William Warfield <179273497+wwarfield@users.noreply.github.com>
2024-10-27 10:17:47 -0500

    split into multi-package project

--- rust/configler-pyo3/src/lib.rs
+++ rust/configler-pyo3/src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn configler_pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
