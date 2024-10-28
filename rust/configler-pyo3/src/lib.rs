7f4a34e9edf9b40c88ee51c60cd1ec0b87721cc0
William Warfield <179273497+wwarfield@users.noreply.github.com>
2024-10-28 13:15:40 -0500

    add type stub generation

--- rust/configler-pyo3/src/lib.rs
+++ rust/configler-pyo3/src/lib.rs
@@ -1,6 +1,8 @@
 use pyo3::prelude::*;
+use pyo3_stub_gen::{define_stub_info_gatherer, derive::gen_stub_pyfunction};
 
 /// Formats the sum of two numbers as string.
+#[gen_stub_pyfunction] 
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok(configler_core::sum_as_string(a, b))
@@ -12,6 +14,9 @@ fn configler_pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
+
+// Define a function to gather stub information.
+define_stub_info_gatherer!(stub_info);
 
 #[cfg(test)]
 mod tests {
