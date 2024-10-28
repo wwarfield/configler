f0c5fa1abc977b775140bfb527eb1e07471f424d
William Warfield <179273497+wwarfield@users.noreply.github.com>
2024-10-27 22:14:46 -0500

    Setup Rust Build Tooling (#2)
    ### Changes
     - Adds gh action for rust code
       - linting
       - formatter
       - test coverage
       - security scanning 
     - Exposes code quality steps as make commands
     - Adds tests for POC
     - Adds templates

--- rust/configler-core/src/lib.rs
+++ rust/configler-core/src/lib.rs
@@ -2,3 +2,14 @@
 pub fn sum_as_string(a: usize, b: usize) -> String {
     (a + b).to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = sum_as_string(3, 2);
+        assert_eq!(result, "5");
+    }
+}
--- rust/configler-pyo3/src/lib.rs
+++ rust/configler-pyo3/src/lib.rs
@@ -1,4 +1,3 @@
-use configler_core;
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -12,4 +11,28 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn configler_pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use pyo3::py_run;
+
+    use super::*;
+
+    #[test]
+    fn test_sum_as_string() {
+        pyo3::append_to_inittab!(configler_pyo3);
+        pyo3::prepare_freethreaded_python();
+
+        Python::with_gil(|py| {
+            let configler_pyo3 = py.import_bound("configler_pyo3").unwrap();
+            py_run!(
+                py,
+                configler_pyo3,
+                r#"
+                assert configler_pyo3.sum_as_string(3,5) == '8'
+            "#
+            )
+        });
+    }
 }
