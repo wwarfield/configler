ffd415be97a847d39ae9de3e6f16112b36b40964
William Warfield <179273497+wwarfield@users.noreply.github.com>
2024-11-11 11:55:03 -0600

    tweaking tests

--- rust/configler-core/src/lib.rs
+++ rust/configler-core/src/lib.rs
@@ -20,6 +20,8 @@ struct EnvironmentConfigSource {}
 
 impl EnvironmentConfigSource {
     fn convert_property_to_environment_name(&self, property_name: &str) -> String {
+        // TODO add more conversion rules
+        // https://smallrye.io/smallrye-config/Main/config/environment-variables/
         str::replace(&property_name.to_uppercase(), ".", "_")
     }
 }
@@ -89,6 +91,8 @@ impl ConfigBuilder {
 
 #[cfg(test)]
 mod tests {
+    use rstest::*;
+
     use super::*;
 
     #[test]
@@ -97,11 +101,19 @@ mod tests {
         assert_eq!(result, "5");
     }
 
-    #[test]
-    fn convert_property_to_environment_name() {
+    #[rstest]
+    #[case("TEST.ONE", "TEST_ONE")]
+    #[case("test.ONE", "TEST_ONE")]
+    #[case("foo", "FOO")]
+    // TODO adding more conversion rules
+    // #[case("foo.\"bar\".baz", "FOO__BAR__BAZ")]
+    // #[case("foo.bar-baz", "FOO_BAR_BAZ")]
+    // #[case("foo.bar[0]", "FOO_BAR_0_")]
+    // #[case("foo.bar[0].baz", "FOO_BAR_0__BAZ")]
+    fn convert_property_to_environment_name(#[case] property_name: String, #[case] expected_env_name: String) {
         let config_source = EnvironmentConfigSource {};
-        let env_name = config_source.convert_property_to_environment_name("test.one");
-        assert_eq!(env_name, "TEST_ONE");
+        let env_name = config_source.convert_property_to_environment_name(&property_name);
+        assert_eq!(expected_env_name, env_name);
     }
 
     #[test]
@@ -114,6 +126,16 @@ mod tests {
         assert_eq!(value.unwrap(), "blah");
 
         env::remove_var("TEST_ONE");
+    }
+
+    #[test]
+    fn use_default_value() {
+        // In this test the environment variable is not set
+        let config = ConfigBuilder::new().add_default_sources().build();
+
+        let default_value = "default_value";
+        let value = config.get_value_or_default("test.two", default_value.to_string());
+        assert_eq!(value, default_value.to_string())
     }
 
     #[test]
