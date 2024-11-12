5fe34dcb84ca14f06d9a2b635a36327bcc930cf3
William Warfield <179273497+wwarfield@users.noreply.github.com>
2024-11-11 18:30:40 -0600

    cover more parsing cases

--- rust/configler-core/src/sources/dot_env.rs
+++ rust/configler-core/src/sources/dot_env.rs
@@ -2,12 +2,11 @@ use super::ConfigSource;
 use std::{collections::HashMap, env, error::Error, str::FromStr};
 
 // https://www.dotenv.org/docs/security/env
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct DotEnvironmentConfigSource {
     // TODO I wonder if there is some value in separating the config source from actually storing
     // config values in a map, hmm
-    values: HashMap<String, String>
+    values: HashMap<String, String>,
 }
 
 impl ConfigSource for DotEnvironmentConfigSource {
@@ -29,7 +28,6 @@ impl ConfigSource for DotEnvironmentConfigSource {
 }
 
 impl DotEnvironmentConfigSource {
-
     #![allow(dead_code)]
     fn convert_property_to_environment_name(&self, property_name: &str) -> String {
         // TODO add more conversion rules
@@ -39,9 +37,8 @@ impl DotEnvironmentConfigSource {
 }
 
 impl FromStr for DotEnvironmentConfigSource {
-
     fn from_str(dot_env_str: &str) -> Result<Self, Self::Err> {
-
+        // TODO break up pipeline into parsing functions?
         let result_key_value_pairs = dot_env_str
             .split("\n")
             // enumerate records into line_no & record
@@ -57,9 +54,16 @@ impl FromStr for DotEnvironmentConfigSource {
             .map(|(line_no, record)| {
                 let tokens = record.split('=').collect::<Vec<&str>>();
                 if tokens.len() < 2 {
-                    return Err(format!("At Line {}, Record has invalid '=' operand", line_no))
+                    return Err(format!(
+                        "At Line {}, Record has invalid '=' operand",
+                        line_no
+                    ));
                 } else {
-                    let key = tokens[0].trim().trim_start_matches("export ").trim();
+                    let key = tokens[0]
+                        .trim()
+                        .trim_start_matches("export ")
+                        .trim()
+                        .to_uppercase();
 
                     let temp_value = tokens[1].trim();
                     let value: &str;
@@ -69,32 +73,48 @@ impl FromStr for DotEnvironmentConfigSource {
                         value = temp_value;
                     }
 
-                    return Ok((key, value))
+                    if key.len() == 0 {
+                        return Err(format!("At Line {}, Key is empty", line_no));
+                    }
+                    if value.len() == 0 {
+                        return Err(format!("At Line {}, value is empty", line_no));
+                    }
+
+                    return Ok((key, value));
                 }
             })
-            .collect::<Vec<Result<(&str, &str), _>>>();
+            .collect::<Vec<Result<(String, &str), _>>>();
 
+        let mut error_message = "".to_owned();
         let mut key_value_map: HashMap<String, String> = HashMap::new();
         for result_pair in result_key_value_pairs {
             if result_pair.is_err() {
-                return Err(result_pair.err().unwrap().to_string())
+                if error_message.len() > 0 {
+                    error_message.push_str("\n");
+                }
+                error_message.push_str(&result_pair.err().unwrap().to_string());
+                // return Err(result_pair.err().unwrap().to_string())
             } else {
                 let pair = result_pair.unwrap();
                 key_value_map.insert(pair.0.to_owned(), pair.1.to_owned());
             }
         }
-        Ok(DotEnvironmentConfigSource {
-            values: key_value_map
-        })
+
+        if error_message.len() > 0 {
+            Err(error_message)
+        } else {
+            Ok(DotEnvironmentConfigSource {
+                values: key_value_map,
+            })
+        }
     }
-    
+
+    // TODO should there be a custom Error Type?
     type Err = String;
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::*;
-
     use super::*;
 
     #[test]
@@ -113,8 +133,17 @@ mod tests {
         assert_eq!(dot_env_source.values.len(), 2);
 
         // verify key values
-        assert_eq!(dot_env_source.values.get("FIRST").map(|s| s.to_string()), Some("one".to_string()));
-        assert_eq!(dot_env_source.values.get("FIRST_FOO").map(|s| s.to_string()), Some("one foo".to_string()));
+        assert_eq!(
+            dot_env_source.values.get("FIRST").map(|s| s.to_string()),
+            Some("one".to_string())
+        );
+        assert_eq!(
+            dot_env_source
+                .values
+                .get("FIRST_FOO")
+                .map(|s| s.to_string()),
+            Some("one foo".to_string())
+        );
     }
 
     #[test]
@@ -138,14 +167,69 @@ mod tests {
         assert_eq!(dot_env_source.values.len(), 7);
 
         // verify key values
-        assert_eq!(dot_env_source.values.get("FIRST").map(|s| s.to_string()), Some("one".to_string()));
-        assert_eq!(dot_env_source.values.get("SECOND").map(|s| s.to_string()), Some("second value".to_string()));
-        assert_eq!(dot_env_source.values.get("SPACE").map(|s| s.to_string()), Some("space value".to_string()));
-        assert_eq!(dot_env_source.values.get("QUOTED").map(|s| s.to_string()), Some("quote value".to_string()));
-        assert_eq!(dot_env_source.values.get("THIRD").map(|s| s.to_string()), Some("third".to_string()));
-        // assert_eq!(dot_env_source.values.get("LOWER").map(|s| s.to_string()), Some("lower value".to_string()));
-        assert_eq!(dot_env_source.values.get("UPPER").map(|s| s.to_string()), Some("UPPER VALUE".to_string()));
+        assert_eq!(
+            dot_env_source.values.get("FIRST").map(|s| s.to_string()),
+            Some("one".to_string())
+        );
+        assert_eq!(
+            dot_env_source.values.get("SECOND").map(|s| s.to_string()),
+            Some("second value".to_string())
+        );
+        assert_eq!(
+            dot_env_source.values.get("SPACE").map(|s| s.to_string()),
+            Some("space value".to_string())
+        );
+        assert_eq!(
+            dot_env_source.values.get("QUOTED").map(|s| s.to_string()),
+            Some("quote value".to_string())
+        );
+        assert_eq!(
+            dot_env_source.values.get("THIRD").map(|s| s.to_string()),
+            Some("third".to_string())
+        );
+        assert_eq!(
+            dot_env_source.values.get("LOWER").map(|s| s.to_string()),
+            Some("fourth value".to_string())
+        );
+        assert_eq!(
+            dot_env_source.values.get("UPPER").map(|s| s.to_string()),
+            Some("UPPER VALUE".to_string())
+        );
+    }
+
+    #[test]
+    fn parse_invalid_dot_env_error() {
+        let dot_env_str = "
+        FIRST=one
+        BAD_FOO=
+        SECOND=la
+        ";
+
+        // Verify parsing error
+        let dot_env_source_result = DotEnvironmentConfigSource::from_str(dot_env_str);
+        assert_eq!(
+            dot_env_source_result.clone().err(),
+            Some("At Line 2, value is empty".to_string())
+        );
+    }
+
+    #[test]
+    fn parse_multiple_dot_env_errors() {
+        let dot_env_str = "
+        FIRST=one
+        BAD_FOO=
+        SECOND=la
+        =blah
+        ";
+
+        // Verify parsing error
+        let dot_env_source_result = DotEnvironmentConfigSource::from_str(dot_env_str);
+        assert_eq!(
+            dot_env_source_result.clone().err(),
+            Some("At Line 2, value is empty\nAt Line 4, Key is empty".to_string())
+        );
     }
 
     // TODO test multiline input
+    //TODO test source name
 }
--- rust/configler-core/src/sources/mod.rs
+++ rust/configler-core/src/sources/mod.rs
@@ -1,6 +1,6 @@
 pub mod config_source;
-pub mod environment;
 pub mod dot_env;
+pub mod environment;
 
 pub use self::config_source::ConfigSource;
 pub use self::environment::EnvironmentConfigSource;
